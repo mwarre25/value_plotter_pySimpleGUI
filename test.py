1970b197e4d5546ad3921e6c2964ea90c24b9a37
Michael Warren <mwarre25@gmail.com>
2019-05-07 14:16:55 -0400

    added test_not_value_lookup

--- test.py
+++ test.py
@@ -2,34 +2,9 @@ import unittest
 
 import value_tool
 
-
-class TestResourcePath(unittest.TestCase):
-    def test_resource_path(self):
-        """
-        Tests resource path
-        """
-        pass
-
-
-# this test doesn't work because of tKinter I think
-# class TestSplash(unittest.TestCase):
-#     def test_splash_screen(self):
-#         """
-#         Tests splash screen
-#         The layout variable used to define the window is a list with length ==
-#         amout of elements in the list.
-
-#         For the splash screen this length should be 1 as there is only one
-#         image in window.
-#         """
-#         layout = value_tool.Splash()
-#         self.assertEqual(len(layout), 1)
-
-
 class TestValueLookup(unittest.TestCase):
     def test_value_lookup(self):
-        """
-        Tests value lookup table
+        """Tests value lookup table
         """
         lookups_dict = {
             (1, 1): 1,
@@ -62,6 +37,40 @@ class TestValueLookup(unittest.TestCase):
         for key, value in lookups_dict.items():
             self.assertEqual(value_tool.value_lookup(key), value)
 
+    def test_not_value_lookup(self):
+        """
+        Tests value lookup table against incorrect values
+        """
+        wrong_lookups_dict = {
+            (1, 1): 6,
+            (1, 2): 6,
+            (1, 3): 7,
+            (1, 4): 8,
+            (1, 5): 8,
+            (2, 1): 6,
+            (2, 2): 6,
+            (2, 3): 7,
+            (2, 4): 9,
+            (2, 5): 9,
+            (3, 1): 6,
+            (3, 2): 7,
+            (3, 3): 8,
+            (3, 4): 10,
+            (3, 5): 10,
+            (4, 1): 7,
+            (4, 2): 8,
+            (4, 3): 9,
+            (4, 4): 10,
+            (4, 5): 10,
+            (5, 1): 7,
+            (5, 2): 9,
+            (5, 3): 10,
+            (5, 4): 10,
+            (5, 5): 10,
+        }
+
+        for key, value in wrong_lookups_dict.items():
+            self.assertNotEqual(value_tool.value_lookup(key), value)
 
 if __name__ == "__main__":
     unittest.main()
